272532b8ef60de4788cdbac9043b4b8d5308c8d2
elvis24 <villaca6@hotmail.com>
2024-08-30 00:09:10 -0500

    feat: add file productController

--- src/main/java/com/aplication/rest/Rest/controller/ProductController.java
+++ src/main/java/com/aplication/rest/Rest/controller/ProductController.java
@@ -1,4 +1,10 @@
 package com.aplication.rest.Rest.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/Product")
 public class ProductController {
 }
